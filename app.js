4bf0ecc7e42500b9d0865ef2610f67b921a2eb42
TbeeTech <tobirammar@gmail.com>
2021-12-30 18:28:41 +0100

    Still optimizing template

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const PORT = 8080
+const express = require ('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const ejs = require('ejs')
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true})
+bodyParser.urlencoded({extended: true})
+app = express();
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+app.listen(PORT, ()=>{
+    console.log('tobadeyDevelopmentServer running @ PORT:: ['+PORT+ ']')
+})
